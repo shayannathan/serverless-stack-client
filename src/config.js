cc404db851289879eb7534ed33f9ecc6bc2e1f0c
Shayan Nathan <shy@Shayans-MacBook-Pro.local>
2021-02-17 13:21:06 -0800

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,18 @@
+const config = {
+    s3: {
+      REGION: "us-east-2",
+      BUCKET: "notes-app-upload-shayan",
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://uk8zl19qge.execute-api.us-east-1.amazonaws.com/prod",
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_9nKwZ5eBC",
+      APP_CLIENT_ID: "3bgt063hf9mcs6smriaepsfo4k",
+      IDENTITY_POOL_ID: "us-east-2:bd7abe7b-0998-40de-8fc9-e56405c3cae5",
+    },
+};
+  
+export default config;
